516a784e2e8946d5873152d9cc7b65ef57fe8a78
unknown <cool.gilmitdinov@mail.com>
2022-05-25 00:14:59 +0300

    added skills image and title

--- src/pages/skills-page/Skills.jsx
+++ src/pages/skills-page/Skills.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import "./Skills.scss";
 
 const Skills = () => {
-
   return (
     <div className="skills">
+      <div className="skills__title">
+        <div className="text-box">
+          <h1>WHAT CAN I DO...</h1>
+          <h1>WHAT CAN I DO...</h1>
+        </div>
+      </div>
+      <div class="skills__image">
+        <div class="skills__image-block">
+          <div class="side -main"></div>
+          <div class="side -left"></div>
+        </div>
+        <div class="skills__image-block">
+          <div class="side -main"></div>
+          <div class="side -left"></div>
+        </div>
+        <div class="skills__image-block">
+          <div class="side -main"></div>
+          <div class="side -left"></div>
+        </div>
+      </div>
     </div>
   );
 };
