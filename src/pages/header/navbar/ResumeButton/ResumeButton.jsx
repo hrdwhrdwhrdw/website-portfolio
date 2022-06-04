69991967b11e451f805fc5c584e08365decc09c3
unknown <cool.gilmitdinov@mail.com>
2022-06-04 17:53:35 +0300

    added resume link

--- src/pages/header/navbar/ResumeButton/ResumeButton.jsx
+++ src/pages/header/navbar/ResumeButton/ResumeButton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./ResumeButton.scss";
+import resume from '../../../../assets/AlbertResume.pdf'
 
 export default function ResumeButton() {
   return (
     <button className="vertical">
-      <span className="text">Resume</span>
+      <a href={resume} target="_blank" className="text" rel="noreferrer">Resume</a>
     </button>
   );
 }
